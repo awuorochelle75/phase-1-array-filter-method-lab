a85f63cb2003ad2967e22b45125a31663877e7d8
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-14 14:15:20 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers=[ {name:'Bobby',hometown:'nevada'},
+                {name:'ken',hometown:'austria'},
+                {name:'Bobby',hometown:'texas'}
+            ]
+function findMatching(drivers,name){
+    return drivers.filter(drivers =>drivers.toLowerCase() === name.toLowerCase())    
+
+}
+function fuzzyMatch(drivers,query){
+    return drivers.filter(drivers => drivers.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+
+
+function matchName(drivers,name){
+    return drivers.filter(drivers => drivers.name === name)
+
+}
+   
